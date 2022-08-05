ead87c008811dce499726f5300d6ce0230d8727a
Irina <irina.a.saunina@gmail.com>
2022-08-05 13:51:35 +0300

    Second_commit

--- coursework2.py
+++ coursework2.py
@@ -4,13 +4,14 @@ from pprint import pprint
 import json
 import os
 
-
 FILE_CATALOG = 'photos'
+os.mkdir(FILE_CATALOG)
 LOG_FILE = 'logs.txt'
 BASE_PATH = os.getcwd()
 FULL_PATH = os.path.join(BASE_PATH, FILE_CATALOG)
 FULL_LOGS_PATH = os.path.join(BASE_PATH, LOG_FILE)
-
+VK_TOKEN = os.getenv("VK_TOKEN")
+YA_TOKEN = os.getenv("YA_TOKEN")
 
 def log_func(file_path, data):
     with open(file_path, 'a', encoding='utf-8') as file:
@@ -18,12 +19,9 @@ def log_func(file_path, data):
         file.write(result)
 
 
-with open('token_vk.txt', 'r') as file_object:
-    token_vk = file_object.read().strip()
-
-
 class VkUser:
     url = 'https://api.vk.com/method/'
+
 
     def __init__(self, token, version):
         self.params = {
@@ -43,7 +41,8 @@ class VkUser:
             info_albums_dict[album['title']] = album['id']
         pprint(info_albums_dict)
 
-    def get_photo(self, owner_id, album_id='profile'):
+
+    def get_photo(self, owner_id, album_id):
         info_photo_list = []
         get_photo_url = self.url + 'photos.get'
         get_photo_params = {
@@ -60,7 +59,7 @@ class VkUser:
             info_photo_dict['file_name'] = f"{photo['likes']['count']}.jpg"
             info_photo_dict['size'] = photo['sizes'][-1]['type']
             info_photo_dict['url'] = photo['sizes'][-1]['url']
-            info_photo_dict['date'] = photo[ 'date']
+            info_photo_dict['date'] = photo['date']
             info_photo_list.append(info_photo_dict)
         with open('info_photo.json', 'w') as file:
             json.dump(info_photo_list, file, ensure_ascii=False, indent=4)
@@ -79,10 +78,6 @@ class VkUser:
         return
 
 
-with open('token_yandex.txt', 'r') as file_object:
-    token_ya = file_object.read().strip()
-
-
 class YaUploader:
     def __init__(self, token_ya):
         self.token = token_ya
@@ -94,11 +89,14 @@ class YaUploader:
         }
 
     def upload(self, disk_file_path: str, file_name: str):
-        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        create_url = "https://cloud-api.yandex.net/v1/disk/resources"
         headers = self.get_headers()
-        params = {"path": disk_file_path, "overwrite": "true"}
-        response = requests.get(upload_url, headers=headers, params=params).json()
-        href = response['href']
+        create_params = {"path": "Photos_from_VK"}
+        requests.put(create_url, headers=headers, params=create_params).json()
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        upload_params = {"path": disk_file_path, "overwrite": "true"}
+        upload_response = requests.get(upload_url, headers=headers, params=upload_params).json()
+        href = upload_response['href']
         photo_name = os.path.join(FULL_PATH, file_name)
         response = requests.put(href, data=open(photo_name, 'rb'))
         response.raise_for_status()
@@ -108,12 +106,14 @@ class YaUploader:
 
 
 if __name__ == '__main__':
-    vk_client = VkUser(token_vk, '5.131')
-    vk_client.get_albums('...')
-    vk_client.get_photo('...', '...')
-    uploader = YaUploader(token_ya)
+    vk_client = VkUser(VK_TOKEN, '5.131')
+    owner_id = input('Введите id пользователя: ')
+    vk_client.get_albums(owner_id)
+    album_id = input('Введите id альбома: ')
+    vk_client.get_photo(owner_id, album_id)
+    uploader = YaUploader(YA_TOKEN)
     photos_list = os.listdir(FULL_PATH)
     for photo in photos_list:
         file_name = f'{photo}'
-        path_to_file = f'Photos from VK/{file_name}'
-        result = uploader.upload(path_to_file, file_name)
+        path_to_file = f'Photos_from_VK/{file_name}'
+        uploader.upload(path_to_file, file_name)
